e833383097c8591e36b02760adb8c6444bc822c2
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-11 10:22:33 -0600

    change structure of project

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
 import PersonalInfo from './components/PersonalInfo.jsx'
 import './index.css'
 
@@ -9,6 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <Header />
-    <PersonalInfo />
+    <MainContent />
   </React.StrictMode>,
 )
