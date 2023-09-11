1c31df4cb3547db2715793024c7223302635217f
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-11 10:21:43 -0600

    create main content container
    container will split the project into left and right sides

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import PersonalInfo from './PersonalInfo'
 
 const MainContent = () => {
   return (
       <>
-        <div className='left-side-content'>
-
+      <div className='content'>
+          <div className='left-side-content'>
+            <PersonalInfo />
         </div>
         <div className='right-side-content'>
             
         </div>
+      </div> 
       </>
   )
 }
