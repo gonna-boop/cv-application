fcf3259f594dce66f82f0b6041eb75d5c05e50af
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-11 10:22:11 -0600

    Create personal info form
    importing to app as component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+// import { FaUserAlt } from "react-icons/fa";
+
 
 const PersonalInfo = () => {
   return (
-    <form>
-
-        <>
-            <div className='genInfo'>General Information</div>
-        </>
-        {/* <label htmlFor="fullName">Full name</label><br />
-        <input type="text" id="fullname" name="fullName" /><br />
-        <label htmlFor="email">Email</label><br />
-        <input type="email" id="email" name="email" />
-        <label htmlFor="phoneNum">Phone Number</label><br />
-        <input type="tel" id="phoneNum" name="phoneNum" />
-        <label htmlFor="address">Address</label><br />
-        <input type="text" id="address" name="address" /> */}
-      </form>
+    <div className='genInfoFull'>
+            <div className='genInfo'>
+                {/* <img className='genInfoLogo' src={FaUserAlt}></img> */}
+                <h2>Personal Information</h2>
+            </div>
+            <form id='form'>  
+                <label htmlFor="fullName">Full name</label>
+                <input type="text" id="fullname" name="fullName" /><br />
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" /><br />
+                <label htmlFor="phoneNum">Phone Number</label>
+                <input type="tel" id="phoneNum" name="phoneNum" /><br />
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" name="address" />
+            </form>
+      </div>
   )
 }
 
-export default PersonalInfo
+export default PersonalInfo;
