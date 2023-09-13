ce5e7dd205e1117923802bc883936d77a60128a8
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-13 10:09:50 -0600

    add education, experience, and user resume components

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import Education from './Education'
+import Experience from './Experience'
 import PersonalInfo from './PersonalInfo'
+import UserResume from './UserResume'
 
 const MainContent = () => {
   return (
@@ -7,9 +10,13 @@ const MainContent = () => {
       <div className='content'>
           <div className='left-side-content'>
             <PersonalInfo />
+            <br></br>
+            <Education />
+            <br></br>
+            <Experience />
         </div>
         <div className='right-side-content'>
-            
+            <UserResume />
         </div>
       </div> 
       </>
