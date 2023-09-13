024ff78a6e9d074731b1746d90b37d5130184f7c
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-13 10:47:49 -0600

    add in live resume changes for education, experience, and finish adding for personal info

--- src/components/UserResume.jsx
+++ src/components/UserResume.jsx
@@ -1,15 +1,31 @@
 import { useAtom, useAtomValue } from 'jotai'
 import React, { useState } from 'react'
-import { eduMessage, globalMessage } from '../store'
+import { globalMessage } from '../store'
 
 const UserResume = () => {
   const message = useAtomValue(globalMessage);
-  const eduMessage2 = useAtomValue(eduMessage);
 
   return (
     <div className='userResume'>
-        <div className='cvHeader'>{message.fullName}{message.email}</div>
-        <div className='test'>{eduMessage2}</div>
+        <div className='cvHeader'>
+        <h2>{message.fullName}</h2>
+        <div>{message.email}</div>
+        <div>{message.phoneNum}</div>
+        <div>{message.address}</div>
+    </div>
+    <div className='educationSection'>
+      <h2>{message.school}</h2>
+      <div>{message.degree}</div>
+      <div>{message.schoolStartDate}</div>
+      <div>{message.schoolEndDate}</div>
+    </div>
+    <div className='experienceSection'>
+        <h3>{message.company}</h3>
+        <div>{message.positionTitle}</div>
+        <div>{message.jobStartDate}</div>
+        <div>{message.jobEndDate}</div>
+        <div>{message.jobDescription}</div>
+    </div>
     </div>
   )
 }
