af80536d3db8dc859969137df6b9c2fe401db3ed
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-09-13 10:10:50 -0600

    begin to add jotai to info to print to user resume

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-// import { FaUserAlt } from "react-icons/fa";
-
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserTie } from '@fortawesome/free-solid-svg-icons'
+import { useAtom } from 'jotai';
+import { globalMessage } from '../store';
 
 const PersonalInfo = () => {
+  const [message, setMessage] = useAtom(globalMessage);
+
+  const handleFullNameChange = (event) => {
+    setMessage({...message, fullName: event.target.value});
+  }
+
+  const handleEmailChange = (event) => {
+    setMessage({...message, email: event.target.value});
+  }
+
   return (
     <div className='genInfoFull'>
             <div className='genInfo'>
-                {/* <img className='genInfoLogo' src={FaUserAlt}></img> */}
+                <FontAwesomeIcon icon={faUserTie} />
                 <h2>Personal Information</h2>
             </div>
             <form id='form'>  
                 <label htmlFor="fullName">Full name</label>
-                <input type="text" id="fullname" name="fullName" /><br />
+                <input type="text" id="fullname" name="fullName" onChange={handleFullNameChange}/><br />
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" /><br />
+                <input onChange={handleEmailChange} type="email" id="email" name="email" /><br />
                 <label htmlFor="phoneNum">Phone Number</label>
                 <input type="tel" id="phoneNum" name="phoneNum" /><br />
                 <label htmlFor="address">Address</label>
                 <input type="text" id="address" name="address" />
             </form>
+            <h2>Message: {message.fullName}</h2>
       </div>
   )
 }
